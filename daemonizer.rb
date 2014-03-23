ca3614cf6423aaef3d04f8f85dcc6e0bcfc956a4
Tyler Hartland <tyler.hartland@code42.com>
2014-03-23 15:12:02 -0500

    daemonizer writes pid immediately after creating file

--- daemonizer.rb
+++ daemonizer.rb
@@ -2,9 +2,9 @@ begin
   Process.daemon(true)
   file_path = ARGV[0]
   pid_file = File.new("#{file_path}.pid", 'w')
+  pid_file.write Process.pid
   $stdout.reopen("#{file_path}.log", 'a')
   $stderr.reopen("#{file_path}.log", 'a')
-  pid_file.write Process.pid
   system("/usr/env/ruby #{file_path}")
 ensure
   File.delete(pid_file)
