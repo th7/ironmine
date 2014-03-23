e517ef027e3c2beb0acfc6004d576014c3c9d732
Tyler Hartland <tyler.hartland@code42.com>
2014-03-23 13:51:10 -0500

    goofy deploy script

--- deploy.rb
+++ deploy.rb
@@ -10,6 +10,9 @@ user         = config[:user]
 username     = site_config[:username]
 password     = site_config[:password]
 
+github_user  = config[:github_user]
+github_repo  = config[:github_repo]
+
 check_ip_url = "#{site_config[:domain]}#{site_config[:dns]}/#{hostname}#{config[:check_ip]}"
 
 get_from     = "#{check_ip_url}?user[username]=#{username}&user[password]=#{password}"
@@ -18,8 +21,18 @@ ip_response  = Net::HTTP.get_response(URI.parse(get_from))
 ip           = ip_response.body
 
 ssh_command  = "ssh #{user}@#{ip}"
-remote_commands = ['ls', 'pwd', 'ls']
+repo_url = "git@github.com:#{github_user}/#{github_repo}.git"
 
-puts `#{ssh_command} '#{remote_commands.join(' && ')}'`
+remote_command = "if [ -d #{github_repo} ]; then
+  cd #{github_repo}
+  git checkout master
+  git pull --no-ff origin master
+else
+  git clone #{repo_url}
+fi
+"
+remote_commands = ["git clone #{repo_url}"]
+
+puts `#{ssh_command} '#{remote_command}'`
 
 # Process.join(pid)
