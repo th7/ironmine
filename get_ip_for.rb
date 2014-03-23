47a4b312f788770a0476b3306a51667db3b5eadb
Tyler Hartland <tyler.hartland@code42.com>
2014-03-23 14:55:01 -0500

    skip cgi escape

--- get_ip_for.rb
+++ get_ip_for.rb
@@ -1,10 +1,9 @@
 require 'net/http'
 require 'yaml'
-require 'cgi'
 
 site_config  = YAML.load(File.read('tylerhartland_config.yml'))
-username     = CGI.escape(site_config[:username])
-password     = CGI.escape(site_config[:password])
+username     = site_config[:username]
+password     = site_config[:password]
 domain       = site_config[:domain]
 dns          = site_config[:dns]
 hostname     = ARGV[0]
