83a85f2bc1ba74773a8d7ea9dead2c5bff28d2e6
Tyler Hartland <tyler.hartland@code42.com>
2014-03-23 12:05:29 -0500

    external_ip_reporter logs and uses separate site config

--- external_ip_reporter.rb
+++ external_ip_reporter.rb
@@ -1,27 +1,48 @@
 require 'yaml'
 require 'net/http'
+require 'logger'
+
+logger = Logger.new(STDOUT)
 
 config = YAML.load(File.read('external_ip_reporter_config.yml'))
+site_config = YAML.load(File.read(config[:site_config]))
 
-username  = config[:username]
-password  = config[:password]
-report_to = config[:report_to]
+username  = site_config[:username]
+password  = site_config[:password]
+dns_url = "#{site_config[:domain]}#{site_config[:dns]}"
 url       = config[:url]
-get_from  = "#{report_to}?user[username]=#{username}&user[password]=#{password}"
+get_from  = "#{dns_url}?user[username]=#{username}&user[password]=#{password}"
 
 loop do
-  ip = Net::HTTP.get(URI.parse(get_from))
+  fresh_ip_response = Net::HTTP.get_response(URI.parse(get_from))
 
-  unless ip == @ip
-    @ip = ip
+  if fresh_ip_response.code =='200'
+    fresh_ip = fresh_ip_response.body
+  else
+    logger.warn("External IP check GET from #{dns_url.inspect} failed.")
+    logger.warn(fresh_ip_response.inspect)
+  end
+
+  unless fresh_ip == @ip
+    logger.info("IP changed from #{@ip.inspect} to #{fresh_ip.inspect}")
+
+
+    @ip = fresh_ip
     params = {
       'user[username]' => username,
       'user[password]' => password,
       'dns[url]' => url,
-      'dns[ip]' => ip
+      'dns[ip]' => @ip
     }
 
-    p = Net::HTTP.post_form(URI.parse(report_to), params)
+    update_response = Net::HTTP.post_form(URI.parse(dns_url), params)
+
+    if update_response.code =='200'
+      logger.info("IP updated. Successful POST to #{dns_url.inspect} with url/key #{url.inspect} and ip #{@ip.inspect}")
+    else
+      logger.warn("IP update to #{dns_url.inspect} failed with key #{url.inspect} and ip #{@ip.inspect}")
+      logger.warn(update_response.inspect)
+    end
   end
 
   sleep 60
