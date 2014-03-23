1caba9a4b6efe78af7dbf378fb10522d8344ded3
Tyler Hartland <tyler.hartland@code42.com>
2014-03-23 11:26:35 -0500

    create basic daemonizer for ruby scripts

---
+++ daemonizer.rb
@@ -0,0 +1,11 @@
+begin
+  Process.daemon(true)
+  file_path = ARGV[0]
+  pid_file = File.new("#{file_path}.pid", 'w')
+  $stdout.reopen("#{file_path}.log", 'a')
+  $stderr.reopen("#{file_path}.log", 'a')
+  pid_file.write Process.pid
+  system("ruby #{file_path}")
+ensure
+  File.delete(pid_file)
+end
